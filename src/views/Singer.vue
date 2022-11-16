cd5b8e23f6c429ff6990930ef148fcf354e3ceff
xing-6464 <xing_6464@foxmail.com>
2022-11-16 14:29:16 +0800

    IndexList 组件基础滚动功能实现

--- src/views/Singer.vue
+++ src/views/Singer.vue
@@ -1,22 +1,36 @@
 <template>
-  <div class="singer">
-    歌手页面
+  <div class="singer" v-loading="!singers.length">
+    <index-list :data="singers"></index-list>
   </div>
 </template>
 
 <script>
 import { getSingerList } from '@/service/singer'
+import IndexList from '@/components/base/index-list/index-list.vue'
 
 export default {
   name: 'singer',
+  components: {
+    IndexList
+  },
+  data () {
+    return {
+      singers: []
+    }
+  },
   async created () {
     const result = await getSingerList()
-    console.log(result)
+    this.singers = result.singers
   }
 }
 
 </script>
 
 <style lang="scss" scoped>
-
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
 </style>
