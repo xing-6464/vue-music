acaeda4f993cf37e4f80351c8cf4b4bdead5a55e
xing-6464 <xing_6464@foxmail.com>
2022-11-16 13:26:59 +0800

    v-loading 自定义指令的开发

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="recommend">
+  <div class="recommend" v-loading="loading">
     <Scroll class="recommend-content">
       <div>
         <div class="slider-wrapper">
@@ -8,7 +8,7 @@
           </div>
         </div>
         <div class="recommend-list">
-          <h1 class="list-title">热门歌单推荐</h1>
+          <h1 class="list-title" v-show="!loading">热门歌单推荐</h1>
           <ul>
             <li
               v-for="item in albums"
@@ -50,6 +50,11 @@ export default defineComponent({
     return {
       sliders: [],
       albums: []
+    }
+  },
+  computed: {
+    loading () {
+      return !this.sliders.length && !this.albums.length
     }
   },
   async created () {
