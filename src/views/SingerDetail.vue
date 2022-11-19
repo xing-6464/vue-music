c4ca46853536a86d509f061044ed1bd4b7400a0b
xing-6464 <xing_6464@foxmail.com>
2022-11-19 19:38:14 +0800

    歌手详情页 MusicList 组件基础代码编写

--- src/views/SingerDetail.vue
+++ src/views/SingerDetail.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="singer-detail">
+    <MusicList
+      :songs="songs"
+      :title="title"
+      :pic="pic"
+    />
   </div>
 </template>
 
 <script>
 import { getSingerDetail } from '@/service/singer'
 import { processSongs } from '@/service/song'
+import MusicList from '@/components/musicList/musicList.vue'
 
 export default {
   name: 'SingerDetail',
   props: {
     singer: Object
   },
+  data () {
+    return {
+      songs: []
+    }
+  },
+  computed: {
+    pic () {
+      return this.singer && this.singer.pic
+    },
+    title () {
+      return this.singer && this.singer.name
+    }
+  },
+  components: {
+    MusicList
+  },
   async created () {
     const result = await getSingerDetail(this.singer)
-    const songs = await processSongs(result.songs)
-    console.log(songs)
+    this.songs = await processSongs(result.songs)
   }
 }
 
