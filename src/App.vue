29f4a2159e20773a3cd61d0aa1774ffd0e426021
xing-6464 <xing_6464@foxmail.com>
2022-11-20 13:58:08 +0800

    播放器基础样式及歌曲播放功能开发

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <m-header></m-header>
   <tab></tab>
   <router-view></router-view>
+  <Player></Player>
 </template>
 
 <script setup>
 import MHeader from '@/components/header/Header.vue'
 import Tab from '@/components/tab/Tab.vue'
+import Player from '@/components/player/player.vue'
 </script>
