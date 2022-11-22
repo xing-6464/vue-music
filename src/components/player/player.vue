3b34735259a2d0994a130e9b894840b8993e1bb5
xing-6464 <xing_6464@foxmail.com>
2022-11-22 12:26:12 +0800

    播放器 中间视图层手指交互相关逻辑开发（上）

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -16,8 +16,16 @@
         <h1 class="title">{{currentSong.name}}</h1>
         <h2 class="subtitle">{{currentSong.singer}}</h2>
       </div>
-      <div class="middle">
-        <div class="middle-l">
+      <div
+        class="middle"
+        @touchstart.prevent="onMiddleTouchStart"
+        @touchmove.prevent="onMiddleTouchMove"
+        @touchend.prevent="onMiddleTouchEnd"
+      >
+        <div
+          class="middle-l"
+          :style="middleLStyle"
+        >
           <div class="cd-wrapper">
             <div
               ref="cdRef"
@@ -38,6 +46,7 @@
         <Scroll
           class="middle-r"
           ref="lyricScrollRef"
+          :style="middleRStyle"
         >
           <div class="lyric-wrapper">
             <div v-if="currentLyric" ref="lyricListRef">
@@ -57,6 +66,10 @@
         </Scroll>
       </div>
       <div class="bottom">
+        <div class="dot-wrapper">
+          <span class="dot" :class="{ 'active': currentShow === 'cd' }"></span>
+          <span class="dot" :class="{ 'active': currentShow === 'lyric' }"></span>
+        </div>
         <div class="progress-wrapper">
           <span class="time time-l">{{ formatTime(currentTime) }}</span>
           <div class="progress-bar-wrapper">
@@ -109,6 +122,7 @@ import useMode from './use-mode'
 import useFavorite from './use-favorite'
 import useCd from './use-cd'
 import useLyric from './use-lyric'
+import useMiddleInteractive from './use-middle-interactive'
 
 // data
 const audioRef = ref(null)
@@ -139,6 +153,14 @@ const {
   playLyric,
   stopLyric
 } = useLyric({ songReady, currentTime })
+const {
+  currentShow,
+  middleLStyle,
+  middleRStyle,
+  onMiddleTouchStart,
+  onMiddleTouchMove,
+  onMiddleTouchEnd
+} = useMiddleInteractive()
 
 // computed
 const playIcon = computed(() => {
